8826d9a55d62d8c97d677e79b7add0488231e5c6
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-04-25 23:43:21 +0600

    basic setup done with necessary packages with vite

---
+++ src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+        ],
+    },
+]);
