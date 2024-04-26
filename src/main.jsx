5d65ad99e35d40ecb53eb22deed2662409045828
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-04-26 13:59:33 +0600

    configured firebase auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes/routes';
+import { HelmetProvider } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+    <ToastContainer/>
+  </React.StrictMode>
 )
