d604dad4758f0a0562c7c1be60d48b355196242c
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-04-29 01:35:57 +0600

    added dummy slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,57 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cube';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Autoplay, Navigation, EffectCube, Pagination } from 'swiper/modules';
 
 const Slider = () => {
 
+    const pagination = { clickable: true, };
 
     return (
-        <div className=''>
-
+        <div className='mx-auto'>
+            <Swiper
+                style={{
+                    '--swiper-navigation-color': 'orange',
+                    '--swiper-pagination-color': 'orange',
+                }}
+                effect={'cube'}
+                grabCursor={true}
+                cubeEffect={{
+                    shadow: false,
+                    slideShadows: false,
+                    shadowOffset: 0,
+                    shadowScale: 0,
+                }}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    pauseOnMouseEnter: false,
+                    disableOnInteraction: false,
+                }}
+                pagination={pagination}
+                navigation={true}
+                modules={[Autoplay, Navigation, EffectCube, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
