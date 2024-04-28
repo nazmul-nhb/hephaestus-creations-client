2170de5f925afbbd3aec467cc71cf838d27d0460
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-04-28 09:32:37 +0600

    created delete button functional

--- src/components/Art/Art.jsx
+++ src/components/Art/Art.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import { TbEdit, TbEditOff } from 'react-icons/tb';
 import { FaStar } from 'react-icons/fa';
+import { RiDeleteBin6Line, RiFileEditFill } from 'react-icons/ri';
 
-const Art = ({ art }) => {
+const Art = ({ art, modifiable, handleDelete }) => {
     const { _id, image, item_name, price, rating, customization, stock_status } = art;
 
     return (
@@ -12,22 +13,34 @@ const Art = ({ art }) => {
             <div className="flex-1">
                 <div className="relative h-full">
                     <img className="rounded-lg w-full h-full" src={image} alt={item_name} />
-                    <h5 title={`${customization ? 'Customizable' : 'Not Customizable'}`} className={`bg-opacity-85 cursor-pointer absolute top-1 right-1 p-1 text-lg font-black rounded-full text-white border ${customization ? 'border-green-600 bg-green-600' : 'border-red-600 bg-red-600'}`}>{customization ? <TbEdit/> : <TbEditOff/>}</h5>
-                    <h3 className={`w-full flex items-center justify-center gap-1 bg-opacity-80 absolute bottom-0 px-1 rounded-b-lg text-sm text-center font-semibold text-white border ${customization ? 'border-[#16a34a] bg-[#16a34a]' : 'border-[#ea580c] bg-[#ea580c]'}`}><span>Ratings: </span>{rating} <FaStar/></h3>
+                    <h5 title={`${customization ? 'Customizable' : 'Not Customizable'}`} className={`bg-opacity-85 cursor-pointer absolute top-1 right-1 p-1 text-lg font-black rounded-full text-white border ${customization ? 'border-green-600 bg-green-600' : 'border-red-600 bg-red-600'}`}>{customization ? <TbEdit /> : <TbEditOff />}</h5>
+                    <h3 className={`w-full flex items-center justify-center gap-1 bg-opacity-80 absolute bottom-0 px-1 rounded-b-lg text-sm text-center font-semibold text-white border ${customization ? 'border-[#16a34a] bg-[#16a34a]' : 'border-[#ea580c] bg-[#ea580c]'}`}><span>Ratings: </span>{rating} <FaStar /></h3>
                 </div>
             </div>
             <div className="flex-1 flex flex-col justify-between gap-2">
                 <h3 className="text-lg font-semibold">{item_name}</h3>
                 <h3 className="text-lg font-bold"><span>Price: </span>${price}</h3>
                 <h4 className="flex-grow"><span>Stock Status: </span>{stock_status}</h4>
-                <Link to={`/details/${_id}`}><Button buttonText={'View Details'} color={'red'} hoverBgColor={'transparent'} hoverColor={'white'} className={'border border-red-700 w-full'}></Button></Link>
+                {
+                    !modifiable ? <Link to={`/details/${_id}`}><Button buttonText={'View Details'} color={'red'} hoverBgColor={'transparent'} hoverColor={'white'} className={'border border-red-700 w-full'}></Button></Link>
+                        : <div className="flex justify-between gap-2">
+                            <button className='flex items-center gap-1'>
+                                <RiFileEditFill /> Update
+                            </button>
+                            <button onClick={()=>handleDelete(_id)} className='flex items-center gap-1'>
+                                <RiDeleteBin6Line /> Delete
+                            </button>
+                        </div>
+                }
             </div>
         </div>
     );
 };
 
-Art.propTypes={
+Art.propTypes = {
     art: PropTypes.object,
+    modifiable: PropTypes.bool,
+    handleDelete: PropTypes.func,
 }
 
 export default Art;
--- src/pages/MyArts/MyArts.jsx
+++ src/pages/MyArts/MyArts.jsx
@@ -2,12 +2,13 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import loader from '../../assets/loader.svg';
 import Art from "../../components/Art/Art";
+import Swal from "sweetalert2";
 
 const MyArts = () => {
     const { user } = useContext(AuthContext);
     const [myArts, setMyArts] = useState([]);
     const [artsLoading, setArtsLoading] = useState(true);
-    console.log(user?.email);
+
     useEffect(() => {
         setArtsLoading(true);
         fetch(`http://localhost:5000/arts/email/${user?.email}`)
@@ -17,6 +18,41 @@ const MyArts = () => {
                 setArtsLoading(false);
             })
     }, [user])
+
+    // Delete from My Art & Crafts
+    const handleDelete = (id) => {
+        const deletableArt = myArts?.find(art => art._id === id);
+        Swal.fire({
+            title: 'Are You Sure?',
+            text: `Delete ${deletableArt.item_name}?
+            You won't be able to revert the process!`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Delete It!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                setArtsLoading(true);
+                fetch(`http://localhost:5000/arts/id/${id}`, { method: "DELETE" })
+                    .then(res => res.json()).then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Art has been deleted.',
+                                'success'
+                            )
+                        const remainingArts = myArts?.filter(art => art._id !== id);
+                        setMyArts(remainingArts);
+                        setArtsLoading(false);
+                        }
+                    })
+            }
+        })
+    }
+
 
     if (artsLoading) {
         return (
@@ -33,6 +69,8 @@ const MyArts = () => {
                     myArts?.map(art => <Art
                         key={art._id}
                         art={art}
+                        modifiable={true}
+                        handleDelete={handleDelete}
                     ></Art>)
                 }
             </div>
